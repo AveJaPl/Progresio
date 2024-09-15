cc35d2104670b9ce6ca10a879c714bd877d1578e
Filip <filip.piatek@spicesolutions.pl>
2024-09-15 18:23:07 +0200

    loading and 404 page

---
+++ progresio/src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-blue-500"></div>
+        <h2 className="text-2xl font-semibold mt-4">Loading...</h2>
+      </div>
+    );
+  }
+  
