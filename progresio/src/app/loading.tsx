9f6451e63b4e13d9d7f12016d5c7f6f9eff6fdc7
Filip <filip.piatek@spicesolutions.pl>
2024-09-15 18:27:30 +0200

    Poprawa strony ładowania

--- progresio/src/app/loading.tsx
+++ progresio/src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-      <div className="flex flex-col items-center justify-center min-[50vh]">
+      <div className="flex flex-col items-center justify-center h-[50vh]">
         <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-blue-500"></div>
         <h2 className="text-2xl font-semibold mt-4">Loading...</h2>
       </div>
