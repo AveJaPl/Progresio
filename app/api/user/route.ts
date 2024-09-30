0a71282fea2c87402f825a72d181d502e30eebda
AveJaPl <filip.piatek2005@gmail.com>
2024-09-30 21:14:39 +0200

    quickfix

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
 import jwt from "jsonwebtoken";
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   try {
     const token = request.cookies.get("token")?.value;
 
