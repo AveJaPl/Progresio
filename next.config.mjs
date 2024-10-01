de312089b3fd6061ee4440dfb6a0248e08144677
AveJaPl <filip.piatek2005@gmail.com>
2024-10-01 22:56:13 +0200

    again pwa changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
+// next.config.mjs
+
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  dest: "public", // Lokalizacja plików wygenerowanych przez PWA
-  disable: process.env.NODE_ENV === "development", // Włączenie PWA tylko w trybie produkcyjnym
-  register: true, // Automatyczna rejestracja Service Workera
-  skipWaiting: true, // Automatyczna aktywacja nowej wersji Service Workera
-  mode: "production",
-  buildExcludes: [/middleware-manifest\.json$/], // Netlify Middleware exclusions
-});
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
+};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
+})(nextConfig);
